ee8f0b10a5e697af49a5ee2a40b30ccc29daa61c
Jake Wagner <jkwagneriii@gmail.com>
2020-12-14 12:35:13 -0600

    setting up standard onClick and onChange functions for the form and input

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Axios from 'axios'
 import './App.css'
 
 
 const App = () => {
+    const [query, setQuery] = useState("")
+
     const APP_ID = "8bacc5bd";
     const APP_KEY = "b60c242d9b21f12794d7e14b797c70ac";
 
@@ -11,15 +13,25 @@ const App = () => {
 
     const getData = async () => {
         const result = await Axios.get(url)
-
         console.log(result)
     }
 
-    getData()
+    const onChange = (event) => {
+        console.log(event.target.value)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        getData();
+    }
 
     return (
         <div className="App">
             <h1>Food Searching App</h1>
+            <form className="search-form" onSubmit={onSubmit}>
+                <input type="text" placeholder="Search Recipes" autocomplete="off" onChange={onChange}/>
+                <input type="submit" value="Search"/>
+            </form>
         </div>
     )
 }
