e85346f420da956681f6a5706769b02b9c4d7e92
Jake Wagner <jkwagneriii@gmail.com>
2020-12-14 12:37:07 -0600

    setState function working properly with the query

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     const APP_ID = "8bacc5bd";
     const APP_KEY = "b60c242d9b21f12794d7e14b797c70ac";
 
-    const url = `https://api.edamam.com/search?q=chicken&amp;app_id=${APP_ID}&amp;app_key=${APP_KEY}`;
+    const url = `https://api.edamam.com/search?q=${query}&amp;app_id=${APP_ID}&amp;app_key=${APP_KEY}`;
 
     const getData = async () => {
         const result = await Axios.get(url)
@@ -17,7 +17,7 @@ const App = () => {
     }
 
     const onChange = (event) => {
-        console.log(event.target.value)
+        setQuery(event.target.value)
     }
 
     const onSubmit = (event) => {
