e37c603ff7dd5af4f7557d96dbbb0ae602116202
Jake Wagner <jkwagneriii@gmail.com>
2020-12-14 13:23:41 -0600

    alert is showing up based on user input

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import Axios from 'axios'
 import {v4 as uuidv4} from 'uuid';
 import './App.css'
 import Recipe from './components/Recipe';
+import Alert from './components/Alert';
 
  
 const App = () => {
     const [query, setQuery] = useState("")
     const [recipes, setRecipes] = useState([]);
+    const [alert, setAlert] = useState("");
 
     const APP_ID = "8bacc5bd";
     const APP_KEY = "b60c242d9b21f12794d7e14b797c70ac";
@@ -15,10 +17,18 @@ const App = () => {
     const url = `https://api.edamam.com/search?q=${query}&amp;app_id=${APP_ID}&amp;app_key=${APP_KEY}`;
 
     const getData = async () => {
-        const result = await Axios.get(url)
-        setRecipes(result.data.hits)
-        console.log(result)
-        setQuery("");
+        if (query !== "") {
+          const result = await Axios.get(url);
+          if(!result.data.more) {
+              return setAlert("That isn't food!")
+          }
+          setRecipes(result.data.hits)
+          console.log(result)
+          setAlert("");
+          setQuery("");  
+        } else {
+            setAlert("Please enter something tasty")
+        }
     }
 
     const onChange = (event) => {
@@ -34,6 +44,7 @@ const App = () => {
         <div className="App">
             <h1>Food Searching App</h1>
             <form className="search-form" onSubmit={onSubmit}>
+                {alert !== "" && <Alert alert={alert}/>}
                 <input 
                 type="text" 
                 placeholder="Search Recipes" autoComplete="off" 
